fad65dedc2c973acf3cecea4e756ff9a4c5b286e
Jupeter <tensor.city@gmail.com>
2018-05-22 13:56:48 +0900

    remove zsl

--- app.js
+++ app.js
@@ -1,32 +1,34 @@
 // package.json, package-lock.json
 // open source 3rd party libraries = lodash(synchronous), RxJS(asynchronous), Cycle.js(functional reactive programming)
-const calc = require('./calc');
+const json = require('./json-exercise/json.js');
 const _ = require('lodash');
 const yargs = require('yargs');
 
 let arg2 = yargs.argv._[0];
-let arg3 = yargs.argv.a;
-let arg4 = yargs.argv.b;
-
-// arguemnts are strings
-// console.log(calc.add(arg3, arg4));
+let arg3 = yargs.argv.name;
+let arg4 = yargs.argv.tel;
 
 
-if (arg2 === 'add') {
-    let result = calc.add(arg3, arg4);
-    console.log(result);
-} else if (arg2 === 'substract') {
-    let result = calc.substract(arg3, arg4);
-    console.log(result);
-} else if (arg2 === 'multiply') {
-    let result = calc.multiply(arg3, arg4);
-    console.log(result);
-} else {
+
+// console.log(arg3, arg4);
+
+//be careful of Scope of variables and functions etc.
+if (arg2 === 'addZsl') {
+    json.addZsl(arg3, arg4);
+} else if (arg2 === 'remove') {
+    let result = json.removeZsl(arg3);
+    let message = result ? `${arg3} was found`: `${arg3} was removed`;
+    console.log(message);
+    
+}
+else {
     console.log("I don't understand what you have input");
 }
 
 // console.log(process.argv);
 // console.log(yargs.argv);
+
+//JSON = JS Object Notation
 
 
 
---
+++ json-exercise/json.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+
+// Assign bank accounts to all Korean people.
+// First of all, all Koreans shall be listed as users of OP.
+// Bank accounts of them are same to their telephone number. 
+// TEL => bank account, IPv6 address
+
+
+let fetchZslList = function() {
+    try {
+        let zslFile = fs.readFileSync("./json-exercise/zsl-list");
+        return JSON.parse(zslFile);
+    } catch(e) {
+        return [];
+    }
+}
+
+let saveZslList = function(parsedJson) {
+    fs.writeFileSync("./json-exercise/zsl-list", JSON.stringify(parsedJson))
+}
+
+
+let addZsl = function (name, tel) {
+    let parsedJson = fetchZslList();
+
+    let zsl = {};
+
+    zsl.name = name;
+    zsl.tel = tel;
+
+    //enumeration
+    let duplicatedZsls = parsedJson.filter((zsl) => zsl.name === name);
+
+    if (duplicatedZsls.length === 0) {
+        parsedJson.push(zsl);
+        saveZslList(parsedJson);
+    }
+}
+let removeZsl = function (name) {
+    let parsedJson = fetchZslList();
+
+
+    //enumeration
+    let newZslList = parsedJson.filter((zsl) => zsl.name !== name);
+    saveZslList(newZslList);
+
+    return newZslList.length === parsedJson.length
+    
+}
+
+// addZsl("Wonderwoman", 821011112222)
+// addZsl("Antman", 821011112222)
+
+// console.log(zsls);
+
+module.exports = {
+    addZsl,
+    removeZsl
+}
+
+
